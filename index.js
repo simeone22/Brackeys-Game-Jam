5e1a95eeae725e4f544b2fa23419de7e801c8d23
Paulpetta668 <48731892+Paulpetta668@users.noreply.github.com>
2022-08-25 14:20:56 +0200

    moduls

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ let express = require('express');
 let http = require('http');
 let app = express();
 
-//app.get()
+//Richiesta pagina home
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/pages/index.html');
 });
+
+
 let server = http.createServer(app);
-server.listen(80);
+server.listen(8080);
